b2492659dd4bf973fd640f3d7443ec5af4b21e35
Newms34 <Newms3450@msn.com>
2015-07-15 14:23:32 -0400

    booms

---
+++ js/boom.js
@@ -0,0 +1,184 @@
+var notBooming = true; //not currently exploding. Generally good thing.
+var firing = false;
+var posX, posY;
+document.onmousemove = function(e) {
+    posX = (e.x || e.clientX)+$(document).scrollLeft();
+    posY = (e.y || e.clientY)+$(document).scrollTop();
+    if (!firing) {
+        $('#lazCont').css({
+            'left': (posX - 54) + 'px',
+            'top': posY + 'px'
+        })
+        $('#reticle').css({
+            'left': (posX - 50) + 'px',
+            'top': (posY - 50) + 'px'
+        })
+
+    }
+
+}
+var updSite=function(){
+	var theSite = $('#website').val();
+	if (theSite.indexOf('http://')!=0 && theSite.indexOf('https://')!=0){
+		theSite = 'http://'+theSite;
+	}
+	console.log(theSite)
+	$('#targ').attr('src',theSite)
+}
+document.onkeyup = function(e) {
+    if (e.which == 66) {
+        boom(100);
+    } else if (e.which == 70) {
+        lazFire(100);
+    }
+}
+var origStats = []
+var avgLeft;
+var avgTop;
+var currScrollTop = 0;
+var currScrollLeft = 0;
+var lazFire = function(amt) {
+    firing = true
+    var tempAmt = amt;
+    $('#lazCont').css('display', 'block')
+    var t = setInterval(function() {
+        //interator scales lazIn from 0-25
+        //and lazOut from 0-55
+        var lazInScale = ((100 - tempAmt) / 100) * 25;
+        var lazOutScale = ((100 - tempAmt) / 100) * 55;
+        var lazOutLeft = 55 - lazOutScale;
+        var lazGloOp =  ((100 - tempAmt) / 100) * .4;
+        $('#lazIn').css({
+            'border-left': lazInScale + 'px solid transparent',
+            'border-right': lazInScale + 'px solid transparent'
+        })
+        $('#lazOut').css({
+            'left': lazOutLeft + 'px',
+            'border-left': lazOutScale + 'px solid transparent',
+            'border-right': lazOutScale + 'px solid transparent'
+        })
+        $('#lazGlo').css('opacity',lazGloOp);//gloooop
+        tempAmt--;
+        console.log(tempAmt)
+        if (!tempAmt) {
+            clearInterval(t);
+            $('#lazCont').css('display', 'none');
+            $('#lazIn').css({
+                'border-left': '0px solid transparent',
+                'border-right': '0px solid transparent'
+            })
+            $('#lazOut').css({
+                'border-left': '0px solid topransparent',
+                'border-right': '0px solid transparent'
+            })
+            $('#lazGlo').css('opacity','0')
+            firing = false;
+            console.log('boom amt ', amt)
+            boom(amt);
+        }
+    }, 20)
+}
+var boom = function(amt) {
+    var n = 10; //number of booms
+    if (notBooming) {
+        origStats = []; //wipe origStats array
+        var allLeft = [];
+        var allTop = [];
+        currScrollTop = $(document).scrollTop();
+        currScrollLeft = $(document).scrollLeft();
+        console.log(currScrollTop)
+        while (n) {
+            var boomBit = document.createElement('div');
+            boomBit.id = 'splode' + n;
+            boomBit.className = 'boomClass';
+            var boomLeft = (Math.floor(Math.random() * 30) - 15) + posX;
+            var boomTop = (Math.floor(Math.random() * 30) - 15) + posY;
+            boomBit.style.left = boomLeft + 'px';
+            boomBit.style.top = boomTop + 'px';
+            allLeft.push(boomLeft);
+            allTop.push(boomTop);
+            //colors!
+            var hue = Math.floor(Math.random() * 25) + 40;
+            var sat = Math.floor(Math.random() * 20) + 80;
+            var val = Math.floor(Math.random() * 30) + 70;
+            origStats.push({
+                h: hue,
+                s: sat,
+                v: val
+            })
+            boomBit.style.backgroundColor = 'hsla(' + hue + ',' + sat + '%,' + val + '%,.99)';
+            var boomSize = Math.floor(Math.random() * 82) + 82;
+            boomBit.style.height = boomSize + 'px';
+            boomBit.style.width = boomSize + 'px';
+            $('#boomCont').append(boomBit);
+            n--;
+        }
+        //now create Shockwaves
+        for (var s = 0; s < 2; s++) {
+            var swDiv = document.createElement('div');
+            swDiv.className = 'shockWave';
+            avgLeft = allLeft.reduce(function(pv, nx) {
+                return pv + nx;
+            }) / allLeft.length;
+            avgTop = allTop.reduce(function(pv, nx) {
+                return pv + nx;
+            }) / allTop.length;
+            swDiv.style.left = avgLeft + 'px';
+            swDiv.style.top = avgTop + 'px';
+            //rotate if 2nd
+            if (s) {
+                swDiv.style.transform = 'rotate(' + (30 + Math.floor(Math.random() * 130)) + 'deg)'
+            }
+            $('#boomCont').append(swDiv);
+            console.log('sw counter', s)
+        }
+
+        notBooming = false;
+        boom(amt);
+    } else {
+        //stuff we need to change: hsla bg color+opacity	
+        //width
+        //height
+        //same (sorta) for shockwaves
+        //also, animate screen 'shake'
+        var shakeMag = (10 * amt / 100);
+        var shakeAmtT = Math.floor(Math.random() * shakeMag);
+        var shakeAmtL = Math.floor(Math.random() * shakeMag);
+        window.scrollTo(shakeAmtL, shakeAmtT)
+        amt = parseInt(amt);
+        for (var i = 0; i < n; i++) {
+            var newHue = (amt / 100) * origStats[i].h;
+            var newSat = (amt / 100) * origStats[i].s;
+            var newVal = (amt / 100) * origStats[i].v;
+            var newHSL = 'hsla(' + newHue + ',' + newSat + '%,' + newVal + '%,' + (amt / 120) + ')';
+            $('#splode' + (i + 1)).css('background-color', newHSL);
+            //color done. Now size. done via scale(n,n), where n is from 1-3
+            var scale = 1 + ((100 - amt) / amt) * 2;
+            if (amt > 5) {
+                $('#splode' + (i + 1)).css('transform', 'scale3d(' + scale + ',' + scale + ',1)')
+            }
+        }
+        //sw!
+        var swScale = 10 + ((100 - amt) / amt) * 50;
+
+        var swHSL = 'hsla(60,20%,' + (amt * 90 / 100) + '%,' + (amt / 100) + ')';
+        //blur width: 50 - 70 and 10-20
+
+        $('.shockWave').css({
+            'box-shadow': '0 0 ' + (50 + (20 * (100 - amt) / amt)) + 'px ' + swHSL + ' inset, 0 0 ' + (10 + (10 * (100 - amt) / amt)) + 'px ' + swHSL,
+            'width': swScale + '%',
+            'height': swScale * .6 + '%',
+            'left': avgLeft - (swScale * 8) + 'px',
+            'top': avgTop - (swScale / 4) + 'px'
+        })
+        amt--;
+        if (amt) {
+            var t = setTimeout(function() {
+                boom(amt);
+            }, 20);
+        } else {
+            notBooming = true;
+            $('#boomCont').empty();
+        }
+    }
+}
--- js/script.js
+++ js/script.js
@@ -1,28 +1,80 @@
 var stopped = false;
+var hyper = false; //in hyperspace?
+var speed = 50; //percent throttle
+var notBooming = true; //not currently exploding. Generally good thing.
 document.onmousemove = function(e) {
     e.x = e.x || e.clientX;
     e.y = e.y || e.clientY;
     if (!stopped) {
-        $('#container').css('transform', 'rotateX(' + e.y + 'deg) rotateY(' + e.x + 'deg)')
+        $('#container').css('transform', 'rotateX(' + e.y + 'deg) rotateZ(' + e.x + 'deg) rotateY(' + (-e.x) + 'deg)')
     }
 }
 
 document.onkeyup = function(e) {
-    if (e.which == 32) {
-        e.preventDefault();
-        stopped ? stopped = false : stopped = true;
+    if (e.which == 72 && !hyper) {
+        //entering hyperspace
+        console.log('hyper!')
+        distChange(30, true);
+    } else if (e.which == 72 && hyper) {
+        //exiting hyperspace
+        console.log('regular!')
+        distChange(30, false);
+    } 
+}
+var distChange = function(amt, dir) {
+    amt = parseFloat(amt) - 0.5;
+    var distAmt;
+    if (dir) {
+        //blur increase
+        distAmt = ((30 - amt) / 10) + 1;
+    } else {
+        distAmt = (amt / 10) + 1;
+    }
+    var xSecAmt = (4 - distAmt) / 4;
+    console.log(distAmt)
+    $('#hype').css('transform', 'scaleY(' + distAmt + ') scaleX(' + xSecAmt + ') scaleZ(' + xSecAmt + ')');
+    if (amt > 0) {
+        var t = setTimeout(function() {
+            distChange(amt, dir);
+        }, 75)
+    } else {
+        hyper ? hyper = false : hyper = true;
+        console.log('set hyper to ', hyper)
     }
 }
-$(document).ready(function(){
-	var allMetal = document.getElementsByClassName('metal');
-	for (var i=0;i<allMetal.length;i++){
-		var hueAmt = Math.floor(Math.random()*10)+95;
-		allMetal[i].style.filter = 'brightness('+hueAmt+'%)';
-		allMetal[i].style.webkitFilter = 'brightness('+hueAmt+'%)';
-	}
-	var engBits = document.getElementsByClassName('engPiece');
-	for (var i=0;i<engBits.length;i++){
-		var rotAmt = i*15;
-		engBits[i].style.transform = 'rotateY('+rotAmt+'deg) translateZ(35px)';
-	}
+
+document.onmousewheel = function(e) {
+    if (e.wheelDelta > 0 && speed <= 95) {
+        //accel
+        speed += 5;
+    } else if (e.wheelDelta < 0 && speed >= 5) {
+        //decel
+        speed -= 5;
+    }
+    var fans = document.getElementsByClassName('engCapF');
+    var spd = (-199 * speed / 10000) + 2;
+    for (var q = 0; q < fans.length; q++) {
+        fans[q].style.webkitAnimation = 'fanSpin ' + spd + 's linear infinite';
+        fans[q].style.animation = 'fanSpin ' + spd + 's linear infinite';
+    }
+    var engGlos = document.getElementsByClassName('engCapB');
+    var gloAmt = (19 * speed / 100) + 1;
+    var col = 'hsl(220,100%,' + ((.6 * speed) + 20) + '%)';
+    for (var m = 0; m < engGlos.length; m++) {
+        engGlos[m].style.boxShadow = '0 0 ' + gloAmt + 'px ' + (gloAmt / 4) + 'px ' + col;
+        engGlos[m].style.backgroundColor = col;
+    }
+}
+$(document).ready(function() {
+    var allMetal = document.getElementsByClassName('metal');
+    for (var i = 0; i < allMetal.length; i++) {
+        var hueAmt = Math.floor(Math.random() * 10) + 95;
+        allMetal[i].style.filter = 'brightness(' + hueAmt + '%)';
+        allMetal[i].style.webkitFilter = 'brightness(' + hueAmt + '%)';
+    }
+    var engBits = document.getElementsByClassName('engPiece');
+    for (var i = 0; i < engBits.length; i++) {
+        var rotAmt = i * 15;
+        engBits[i].style.transform = 'rotateY(' + rotAmt + 'deg) translateZ(35px)';
+    }
 })
